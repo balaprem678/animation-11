65d372bb40003f9896b6a942bb3e117d0139d257
prem568 <balaprem568@gmail.com>
2022-06-29 16:21:18 +0530

    add.

---
+++ main.js
@@ -0,0 +1,35 @@
+function startAD(){
+	setTimeout(function(){
+			document.getElementById("plane").style.transform='translateX(0) translateY(0)';
+			document.getElementById("plane").style.transition='all 1 ease';
+      document.getElementById("plane").style.opacity='1';
+		},250)
+		setTimeout(function(){
+		
+		},750)
+		setTimeout(function(){
+			
+		},1250)
+		setTimeout(function(){
+
+		},1750)
+		setTimeout(function(){
+		
+		},2250)
+		setTimeout(function(){
+		
+		},3250)
+		setTimeout(function(){
+		
+		},3250)
+		setTimeout(function(){
+		
+		},3750)
+		setTimeout(function(){	
+		
+		},4250)
+		setTimeout(function(){
+		
+		},4750)
+	
+}Window.load=startAD();
